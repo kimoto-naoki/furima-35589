bc3fe0ec1c098ba02a77b4fc2eb08b6e3366e567
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-08 21:42:45 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthday, presence: true
+   
+  validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}  
+  validates :ruby_last_name, :ruby_first_name, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :password,  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
 end
