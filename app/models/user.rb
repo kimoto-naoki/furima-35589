bd0f0cc1c9ebc13f5f104049d91774a6ca7939a3
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-09 14:46:29 +0900

    修正点の反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthday, presence: true
    
-  validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}  
+  validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}  
   validates :ruby_last_name, :ruby_first_name, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :password,  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
 end
