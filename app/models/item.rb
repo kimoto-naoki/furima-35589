02e2fd550a81dfe1ce55d3dfa9951520ecdce787
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-10 12:12:30 +0900

    モデルファイル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  vailidates :item_name, :item_text, :category_id, :condition_id, :who_cost_id, 
+  validates :item_name, :item_text, :category_id, :condition_id, :who_cost_id, 
             :shipping_area_id, :days_to_ship_id, :price, :user ,:image,       presence: true
 
   validates :price, inclusion: { in: 300..9999999 }
-  validates :price, fomat: { with: /\A[0-9]+\z/ }
+  validates :price, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
   has_one_attached :image
