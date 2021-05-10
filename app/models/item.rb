58a584418cdb0a5fb744196ffa888ed6febcce7d
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-10 16:25:47 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,18 @@ class Item < ApplicationRecord
   validates :item_name, :item_text, :category_id, :condition_id, :who_cost_id, 
             :shipping_area_id, :days_to_ship_id, :price, :user ,:image,       presence: true
 
+  validates :category_id, numericality: { other_than: 1 } 
+  
   validates :price, inclusion: { in: 300..9999999 }
   validates :price, format: { with: /\A[0-9]+\z/ }
+  
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :who_cost
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
 end
---
+++ spec/factories/who_costs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :who_cost do
+    
+  end
+end
