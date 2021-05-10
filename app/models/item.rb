935e655bde8eac4df351a6141af47f7b7a1a63df
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-10 11:11:04 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  vailidates :item_name, :item_text, :category_id, :condition_id, :who_cost_id, 
+            :shipping_area_id, :days_to_ship_id, :price, :user ,:image,       presence: true
+
+  validates :price, inclusion: { in: 300..9999999 }
+  validates :price, fomat: { with: /\A[0-9]+\z/ }
 end
