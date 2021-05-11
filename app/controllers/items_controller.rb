b34eebdc5f8ce1564b79e24211b33fab4daa3f50
kimoto-naoki <82770704+kimoto-naoki@users.noreply.github.com>
2021-05-12 01:43:43 +0900

    修正点の反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only:[:show, :update]
+  before_action :move_to_root, only:[:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,28 +21,36 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.find(params[:id])
+    
   end
 
   def edit
-    if current_user.id != Item.find(params[:id]).user_id
-      redirect_to root_path
-    end
-    @items = Item.find(params[:id])
+    set_item
   end
 
   def update
-    @items = Item.find(params[:id])
+    set_item
     if @items.update(item_params)
       redirect_to action: :show
     else
       render :edit
     end
   end
+  
   private
   def item_params
     params.require(:item).permit(
       :item_name, :item_text, :category_id, :condition_id, :who_cost_id,
       :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @items = Item.find(params[:id])
+  end
+
+  def move_to_root
+    if current_user.id != Item.find(params[:id]).user_id
+      redirect_to root_path
+    end
   end
 end
